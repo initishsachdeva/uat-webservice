45af75865bc6bdcaac3a781348c0f650b4cb90df
nitsachdeva <nitsachdeva@deloitte.com>
2022-12-24 15:21:23 +0530

    first commit to uat proj

---
+++ src/main/java/com/webservice/uatwebservice/UatWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.uatwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UatWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UatWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservice/uatwebservice/UatWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.uatwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UatWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
